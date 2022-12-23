f58ffa1aaca5175d8cfc2e6bb8de3ebd6031b42e
fdragons <fdragons@nexg.net>
2022-12-23 09:03:28 +0900

    Initial commit

---
+++ ScmSmsc/src/main/java/net/nexg/scm/scmsmsc/LocationHelper.java
@@ -0,0 +1,15 @@
+package net.nexg.scm.scmsmsc;
+
+import android.location.Location;
+
+public class LocationHelper {
+
+    public static float getDistanceInKM(double startLatitude, double startLongitude, double endLatitude, double endLongitude) {
+
+        float[] result = new float[0];
+
+        Location.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude, result);
+
+        return result[0] / 1000;
+    }
+}
